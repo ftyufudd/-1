58add926b3faed720c0ae9953fc52aa9eb1ad01d
ftyufudd <halikovaimilia@gmail.com>
2025-09-11 00:33:32 +0500

    Add files via upload

---
+++ Практическая 2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pr2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 1
+            //string input = Console.ReadLine();
+            //int result =
+            //Convert.ToInt32(input, 2);
+            //Console.Write(result);
+
+            //Задание 2
+            Console.WriteLine();
+            string input = Console.ReadLine();
+            int number = int.Parse(input);
+            string result = Convert.ToString(number, 2);
+            Console.WriteLine(result);
+        }
+    }
+}
