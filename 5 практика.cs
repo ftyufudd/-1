b0684e018b8f0873da78f66463ba4b79f299e88b
ftyufudd <halikovaimilia@gmail.com>
2025-09-18 09:23:27 +0300

    Add files via upload

---
+++ 5 практика.cs
@@ -0,0 +1,51 @@
+﻿//        Console.Write("Введите число: ");
+//        int n = int.Parse(Console.ReadLine());
+//
+//        while (n < 6)
+//        {
+//            Console.WriteLine("n = " + n);
+//            n++;
+//        }
+//
+//
+//    Console.Write("Введите число: ");
+//    int n = int.Parse(Console.ReadLine());
+//    do
+//    {
+//    Console.WriteLine("n = " + n);
+//    n++;
+//    }
+//    while (n < 6);
+//    
+/////////
+
+//        Console.Write("Введите число: ");
+//        int i = int.Parse(Console.ReadLine());
+//
+//        int value = 1;
+//        while (value <= i)
+//        {
+//            Console.Write(value + " ");
+//            value *= 2;
+//        }
+////////
+
+//       Console.Write("Введите кол-во чисел Фибоначчи: ");
+//       int n = int.Parse(Console.ReadLine());
+//
+//       if (n <= 0)
+//       {
+//           Console.WriteLine("Введите положительное число");
+//           return;
+//       }
+//
+//       int a = 0, b = 1;
+//
+//       for (int i = 0; i < n; i++)
+//       {
+//           Console.Write(a + " ");
+//           int temp = a + b;
+//           a = b;
+//           b = temp;
+//       }
+//
